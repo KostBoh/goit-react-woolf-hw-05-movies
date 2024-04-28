4c5422fa7ea90e726cefaf945f10d6c952b5e6c6
KostBoh <bohdan.kostya@gmail.com>
2024-04-28 22:38:15 +0300

    add changes first watch

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,25 @@
+import Home from 'pages/Home';
+import MovieDetails from 'pages/MovieDetails';
+import Movies from 'pages/Movies';
+import { Navigate, Route, Routes } from 'react-router-dom';
+import Header from './Header/Header';
+import Cast from './Cast/Cast';
+import Reviews from './Reviews/Reviews';
+
 export const App = () => {
   return (
-    <div
-      style={{
-        height: '20vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101',
-      }}
-    >
-      React homework template
-    </div>
+    <>
+      <h2>App</h2>
+      <Routes>
+        <Route path="/" element={<Header />}>
+          <Route index element={<Home />} />
+          <Route path="/movies" element={<Movies />} />
+          <Route path="/movies/:movieId" element={<MovieDetails />} />
+          <Route path="/movies/:movieId/cast" element={<Cast />} />
+          <Route path="/movies/:movieId/reviews" element={Reviews} />
+        </Route>
+        <Route path="*" element={<Navigate to="/" />} />
+      </Routes>
+    </>
   );
 };
---
+++ src/components/Cast/Cast.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const cast = () => {
+  return <div>cast</div>;
+};
+
+export default cast;
