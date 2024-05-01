3cddd45f02ee9cc10b90b7d0c44d60c6a8186894
KostBoh <bohdan.kostya@gmail.com>
2024-05-01 19:19:27 +0300

    add change movies list search

--- src/pages/Movies.jsx
+++ src/pages/Movies.jsx
@@ -1,14 +1,40 @@
+import Loader from 'components/Loader/Loader';
+import MovieList from 'components/MovieList/MovieList';
 import SearchForm from 'components/SearchForm/SearchForm';
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { useSearchParams } from 'react-router-dom';
+import { searchMovies } from 'service/movie-service';
 
 const Movies = () => {
-  const handleSearchSubmit = value => {
-    console.log(value);
+  const [searchParams, setSearchParams] = useSearchParams();
+  const [movies, setMovies] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState('');
+
+  const handleSearchSubmit = movie => {
+    setSearchParams({ movie });
   };
+
+  useEffect(() => {
+    const movie = searchParams.get('movie');
+    if (!movie) {
+      return;
+    }
+
+    searchMovies(movie)
+      .then(setMovies)
+      .catch(({ message }) => setError(message))
+      .finally(() => {
+        setIsLoading(false);
+      });
+  }, [searchParams]);
 
   return (
     <>
       <SearchForm onSubmit={handleSearchSubmit} />
+      {isLoading && <Loader />}
+      <MovieList movies={movies} />
+      {error && <h2>Something went wrong...</h2>}
     </>
   );
 };
